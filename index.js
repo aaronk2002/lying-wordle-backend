9d4db7f3518c563cfcdf23e9e7ee5224dddb950a
aaronk2002 <aaronakj2002@gmail.com>
2022-07-21 12:44:27 -0400

    Actually Allow Netlify App to access API's 4 and make simpler API

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require("mongoose");
-const wordModel = require("./models/WordModel.js");
-
 const app = express();
-const port = 3001;
+
+
+//Cors Configuration
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
     res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
     )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
+    return res.status(200).json({})
+  }
+  next()
 })
 
+
+// Mongoose Connection
+const mongoose = require("mongoose");
 mongoose.connect('mongodb+srv://aaron25:7bFEmCUlpcgveK73@cluster0.3q4fpgn.mongodb.net/?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }
 );
-
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
     console.log("Connected successfully");
 });
 
-// Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-app.get('/details', async (req, res) => {
-    const user = await wordModel.find({});
-    res.send(user);
+
+app.get('/', (req, res) => {
+    res.send({data: 'Hello World, from express'});
 });
-
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
