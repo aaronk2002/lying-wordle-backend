e945849ef9152ab8c312a9ea13804887f62e5e79
aaronk2002 <aaronakj2002@gmail.com>
2022-07-21 12:24:47 -0400

    Actually Allow Netlify App to access API's 3

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const mongoose = require("mongoose");
 const wordModel = require("./models/WordModel.js");
 
 const app = express();
 const port = 3001;
-
-app.use(cors({
-    origin: 'https://lie-wordle.netlify.app'
-}));
 
 mongoose.connect('mongodb+srv://aaron25:7bFEmCUlpcgveK73@cluster0.3q4fpgn.mongodb.net/?retryWrites=true&w=majority',
     {
