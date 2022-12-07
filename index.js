478ac9c7900e108d7fbea642027e79a8ec4481a7
aaronk2002 <aaronakj2002@gmail.com>
2022-12-07 08:36:47 -0500

    Fix CORS Attempt 6

--- index.js
+++ index.js
@@ -19,13 +19,36 @@ const corsOptions = {
 };
 app.use(cors(corsOptions)); */
 
-app.use(function (req, res, next) {
+/* app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
-});
+}); */
+
+const corsMiddleware = (req, res, next) => {
+  res = applyCorsHeaders(res);
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  next()
+}
+
+const applyCorsHeaders = res => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // or res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  return res;
+}
+
+app.use(corsMiddleware);
 
 /* var corsOptions = {
   origin: "https://lie-wordle.netlify.app",
