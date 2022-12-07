2fe0e7c9a5d01536521dd46b87949d5802d64bc1
aaronk2002 <aaronakj2002@gmail.com>
2022-12-07 07:42:03 -0500

    Fix CORS Attempt 1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 
 // Cors Configuration
-const whitelist = ["https://lie-wordle.netlify.app"];
+/* const whitelist = ["https://lie-wordle.netlify.app", "http://localhost:3000"];
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
@@ -16,13 +16,26 @@ const corsOptions = {
     }
   },
   credentials: true,
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions)); */
+
+/* this.app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+}); */
+
+var corsOptions = {
+  origin: "*",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 
 // Mongoose Connection
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_URI,
+mongoose.connect(/* process.env.DB_URI */ 'mongodb+srv://aaron25:7bFEmCUlpcgveK73@cluster0.3q4fpgn.mongodb.net/?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
